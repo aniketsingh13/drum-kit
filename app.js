641266e10ec987ae38bf8949ec308e154d81b0dd
aniketsingh13 <85512784+aniketsingh13@users.noreply.github.com>
2021-08-31 03:09:13 -0700

    updated app.js and add sound folder

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-for (var i=0; document.querySelectorAll(".drum").length; i++){
-document.querySelectorAll(".drum")[i].addEventListener("click",function (){
-    alert("click");
-});
+for (var i = 0; document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+      
+    });
 
 }
