7498d0a7478b8b6c106ba17425230021c5244691
aniketsingh13 <85512784+aniketsingh13@users.noreply.github.com>
2021-08-31 03:30:24 -0700

    update app.js and adding image folder

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 for (var i = 0; document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-      
+      var audio= new Audio("sounds/tom-1.mp3");
+      audio.play();
+
     });
 
 }
